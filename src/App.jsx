1c4fc653e5cfa32fa0aab515f04e681cba8e2437
nam <johndoe@example.com>
2024-03-11 09:52:24 +0700

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import MainLayout from 'src/components/MainLayout'
 import AuthLayout from 'src/components/AuthLayout'
-import CheckoutRoute from './routes/CheckoutRoute'
-import CreateShowTime from './modules/AdminMovie/pages/CreateShowTime'
+import CheckoutRoute from 'src/routes/CheckoutRoute'
+import CreateShowTime from 'src/modules/AdminMovie/pages/CreateShowTime'
 import AdminLayout from 'src/components/AdminLayout'
-import Success from './modules/Ticket/Success'
-import FilmDetail from './modules/Home/components/FilmDetail'
+import Success from 'src/modules/Ticket/Success'
+import FilmDetail from 'src/modules/Home/components/FilmDetail'
 import Purchase from 'src/modules/Home/components/Purchase'
-import Login from './modules/Authentication/pages/Login'
-const User = lazy(() => import('./modules/AdminMovie/pages/User'))
+import Login from 'src/modules/Authentication/pages/Login'
+const User = lazy(() => import('src/modules/AdminMovie/pages/User'))
 
 // Không import trực tiếp các pages, vì nó sẽ được tải tất cả ở lần đầu tiên
 // import Home from "modules/Home/pages/Home";
@@ -19,13 +19,13 @@ const User = lazy(() => import('./modules/AdminMovie/pages/User'))
 // import Register from "modules/Authentication/pages/Register";
 
 // Để chỉ cần tải những pages cần thiết ta sử dụng kĩ thuật lazyload
-const Home = lazy(() => import('./modules/Home/pages/Home'))
-const Movie = lazy(() => import('./modules/Movie/pages/Movie'))
-// const Login = lazy(() => import('./modules/Authentication/pages/Login'))
-const Register = lazy(() => import('./modules/Authentication/pages/Register'))
+const Home = lazy(() => import('src/modules/Home/pages/Home'))
+const Movie = lazy(() => import('src/modules/Movie/pages/Movie'))
+// const Login = lazy(() => import('src/modules/Authentication/pages/Login'))
+const Register = lazy(() => import('src/modules/Authentication/pages/Register'))
 
-const MovieList = lazy(() => import('./modules/AdminMovie/pages/MovieList'))
-const AddMovie = lazy(() => import('./modules/AdminMovie/pages/AddMovie'))
+const MovieList = lazy(() => import('src/modules/AdminMovie/pages/MovieList'))
+const AddMovie = lazy(() => import('src/modules/AdminMovie/pages/AddMovie'))
 
 function App() {
   return (
